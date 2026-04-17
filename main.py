e76af73bb402828c3a021e2ee30e53be1fb0d71f
aiuc209 <dzuiryuomdbaoy@gmail.com>
2026-04-17 22:26:49 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+Chiqish Hxux
+Topic: Chiqish:
+"""
+
+def main():
+    print("Welcome to Chiqish Hxux!")
+    print("Muvaffaqiyatli ishga tushirildi.")
+
+if __name__ == "__main__":
+    main()
